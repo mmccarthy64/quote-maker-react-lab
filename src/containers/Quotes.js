857100223641982af70409ce528023bfbc0b8393
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-20 15:01:56 -0500

    commit tree a80e3b953340799eb49b5f3a89e6bb2ebdc3913d

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeQuote, downvoteQuote, upvoteQuote } from '../actions/quotes';
 import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
@@ -15,11 +16,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              <QuoteCard removeQuote={removeQuote} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote} />
             </div>
           </div>
         </div>
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,18 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      let quote = { id: uuid(), content: action.content, author: action.author }
+      return { ...state, quotes: [...state.quotes, quote] }
+    case 'REMOVE_QUOTE':
+      let quotes = state.quotes.filter( quote => quote.id !== action.id )
+      return { quotes }
+    case 'UPVOTE_QUOTE':
+      return 
+    case 'DOWNVOTE_QUOTE':
+      return 
+    default:
+      return state
+  }
 }
